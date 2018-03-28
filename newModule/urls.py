56b888ef5eba8aa0d325bd512f91d3acf6a150b5
nikolay.kroshko <kroshko@codevog.com>
2018-03-28 18:22:02 +0300

    working on crud

--- newModule/urls.py
+++ newModule/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='show'),
-    path('<int:user_id>/', detail, name='detail')
+    path('<int:user_id>/', detail, name='detail'),
+    path('<int:user_id>/edit/', edit, name='edit'),
+    path('create/', create, name='create')
 ]
--- newModule/views.py
+++ newModule/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Users
+from .forms import UserForm
 
 
 def mainIndex(request):
@@ -13,6 +14,32 @@ def index(request):
 
 
 def detail(request, user_id):
+    print('detail')
+    print(user_id)
     user = get_object_or_404(Users, pk=user_id)
     return render(request, 'list/details.html', {'user': user})
+
+
+def create(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('/list/' + str(user.pk))
+    else:
+        form = UserForm()
+        return render(request, 'list/create.html', {'form': form})
+
+
+def edit(request, user_id):
+    print(request.method)
+    user = get_object_or_404(Users, pk=user_id)
+    if request.method == "PATCH":
+        user = UserForm(request.PATCH, instance=user)
+        if user.is_valid():
+            user.save()
+            return redirect('/list/' + str(user.pk))
+    else:
+        user = UserForm(instance=user)
+        return render(request, 'list/edit.html', {'form': user})
 
